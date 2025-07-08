f80dbef1dea012d2261424f62dfe36a32657e7c7
Srikanth-Krishna <srikanth.krishna1398@gmail.com>
2025-07-08 23:35:07 +0530

    refined navbar

--- src/component/Main Nav/MainNavigation.js
+++ src/component/Main Nav/MainNavigation.js
@@ -7,11 +7,49 @@ import {
   FaThLarge,
 } from 'react-icons/fa';
 import './MainNavigation.css';
-import { MenuUnfoldOutlined, MenuFoldOutlined } from '@ant-design/icons';
+import {
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  SettingOutlined,
+  UserOutlined,
+  InboxOutlined,
+  MoneyCollectOutlined,
+  LogoutOutlined,
+} from '@ant-design/icons';
 import { Header } from 'antd/es/layout/layout';
-import { Avatar, Badge, Button } from 'antd';
+import { Avatar, Badge, Button, Dropdown } from 'antd';
+import { Link } from 'react-router-dom';
 
 export default function MainNavigation({ collapsed, setCollapsed }) {
+  const items = [
+    {
+      key: '1',
+      label: 'My Account',
+      icon: <UserOutlined />,
+      disabled: false,
+    },
+    {
+      key: '2',
+      label: 'Inbox',
+      icon: <InboxOutlined />,
+    },
+    {
+      key: '3',
+      label: 'Billing',
+      icon: <MoneyCollectOutlined />,
+    },
+    {
+      key: '4',
+      label: 'Settings',
+      icon: <SettingOutlined />,
+    },
+    {
+      key: '5',
+      label: <Link to='/users/login'>Logout</Link>,
+      icon: <LogoutOutlined />,
+    },
+  ];
+
   return (
     <Header className='app-header'>
       <div className='header-left'>
@@ -34,7 +72,11 @@ export default function MainNavigation({ collapsed, setCollapsed }) {
 
       <div className='header-right'>
         <button className='circle-btn'>
-          <FaRegMoon />
+          <Avatar
+            style={{ backgroundColor: '#f3f4f6', color: 'black' }}
+            size='default'
+            icon={<FaRegMoon />}
+          ></Avatar>
         </button>
 
         <button className='circle-btn badge'>
@@ -56,7 +98,6 @@ export default function MainNavigation({ collapsed, setCollapsed }) {
               icon={<FaRegCommentDots />}
             ></Avatar>
           </Badge>
-
           {/* <span className='badge-count'>1</span> */}
         </button>
 
@@ -68,17 +109,20 @@ export default function MainNavigation({ collapsed, setCollapsed }) {
           <FaThLarge />
         </button>
 
-        <div className='profile'>
-          <img
-            src='https://i.pravatar.cc/40?img=5'
-            alt='Kristin Watson'
-            className='avatar'
-          />
-          <div className='profile-info'>
-            <span className='name'>Kristin Watson</span>
-            <small className='role'>Admin</small>
+        <Dropdown menu={{ items }} trigger={['click']}>
+          <div className='profile'>
+            <img
+              src='https://i.pravatar.cc/40?img=5'
+              alt='Kristin Watson'
+              className='avatar'
+            />
+
+            <div className='profile-info'>
+              <span className='name'>Kristin Watson</span>
+              <small className='role'>Admin</small>
+            </div>
           </div>
-        </div>
+        </Dropdown>
       </div>
     </Header>
   );
